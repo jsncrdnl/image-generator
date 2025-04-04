b288e80def383c9518882f9b8d829a18acd9b0d1
jsn.crdnl <jsn.crdnl@gmail.com>
2025-04-04 13:45:54 +0200

    fixed generate.sh line endings & fixed generate.bat dir creation

--- generate.bat
+++ generate.bat
@@ -1,3 +1,3 @@
-mkdir output_images
+if not exist output_images mkdir output_images
 docker build -t img-generator .
 docker run --rm -v .\output_images:/app/output_images -v .\input.txt:/app/input.txt -v .\.env:/app/.env img-generator
